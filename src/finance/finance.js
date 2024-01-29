1108724f455131c809d0f614a4b980f5e512ad91
Arbina001 <arbina.ghanchi@adrixus.com>
2024-01-29 14:19:47 +0530

    fix space

--- src/finance/finance.js
+++ src/finance/finance.js
@@ -41,8 +41,7 @@ return (
               </Contectusbutton>
             </Financeleftbox>
             <Financerightbox>
-              <Headingbox>
-                
+              <Headingbox>               
                 <Headingstyled>EXCELLENT ADVICE</Headingstyled>
               </Headingbox>
               <Servicewrapper>
